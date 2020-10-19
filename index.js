512a90d39d4613f07a4e0a6794f292415d476a72
Ola Ouso <ouso.ola@gmail.com>
2020-10-19 18:14:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+function mapToNegativize(numbers) {
+    return numbers.map(number => number * -1);
+}
+
+function mapToNoChange(array) {
+    return array.map(elem => elem);
+}
+
+function mapToDouble(numbers) {
+    return numbers.map(number => number * 2);
+}
+
+function mapToSquare(numbers) {
+    return numbers.map(number => number * number);
+}
+
+function reduceToTotal(numbers, startingNumber=0) {
+    return numbers.reduce(((acc, cur) => acc + cur), startingNumber); 
+}
+
+function reduceToAllTrue(values) {
+    return values.reduce((acc, cur) => {
+        if (!cur || !acc) {
+            return false;
+        }
+
+        return true;
+    }, true);
+}
+
+function reduceToAnyTrue(values) {
+    return values.reduce((acc, cur) => {
+        if (!!cur || !!acc) {
+            return true;
+        }
+
+        return false;
+    }, false);
+}
